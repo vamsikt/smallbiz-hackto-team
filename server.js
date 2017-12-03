ac73545b945ec91cbae594e2512e5142221c7a81
Vamsi <vkt@Vamsis-MacBook-Pro.local>
2017-12-02 22:58:15 -0500

    initial code clean

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 "use strict";
-var config = require('./config.json')
 
-const PAGE_ACCESS_TOKEN =config.page_access_token;
-const APIAI_TOKEN = "8ab4f1896fb047ea95af3fbf24ccfb72";
-const FB_VALIDATION_TOKEN = "smallbizTo";
+//all config params
+const config = require("./config.json");
+const PAGE_ACCESS_TOKEN = config.page_access_token;
+const APIAI_TOKEN = config.api_ai_token;
+const FB_VALIDATION_TOKEN = config.fb_validation_token;
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const apiai = require("apiai");
 const moment = require("moment-timezone");
-
-
-var qb_client_id=config.clientId;
-
-
+const qb_client_id = config.clientId;
+const qb_url = config.api_uri + config.realmId;
 const app = express();
+const apiaiApp = apiai(APIAI_TOKEN);
+
 app.set("port", process.env.PORT || 5000);
 let currentTime = moment();
 let estTimeStamp = moment.tz(currentTime, "America/Toronto").format();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Server start
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log(
     "Express server listening on port %d in %s mode",
@@ -29,12 +30,12 @@ const server = app.listen(process.env.PORT || 5000, () => {
   );
 });
 
-const apiaiApp = apiai(APIAI_TOKEN);
-
+/*just static home page */
 app.get("/", (req, res) => {
   console.log("Time Stamp :" + estTimeStamp);
   res.send("Home Page");
 });
+
 /* For Facebook Validation */
 app.get("/webhook", function(req, res) {
   if (
@@ -49,12 +50,14 @@ app.get("/webhook", function(req, res) {
   }
 });
 
+/* handle fb webhook incoming messages */
 app.post("/webhook", function(req, res) {
   // You must send back a status 200 to let the Messenger Platform know that you've
   // received the callback. Do that right away because the countdown doesn't stop when
   // you're paused on a breakpoint! Otherwise, the request might time out.
   res.sendStatus(200);
 
+  //req body from fb
   var data = req.body;
 
   // Make sure this is a page subscription
@@ -66,7 +69,6 @@ app.post("/webhook", function(req, res) {
 
       // iterate over each messaging event
       pageEntry.messaging.forEach(function(messagingEvent) {
-
         let propertyNames = [];
         for (var prop in messagingEvent) {
           propertyNames.push(prop);
@@ -120,7 +122,7 @@ function receivedPostback(event) {
     timeOfPostback
   );
 
-  sendButtonMessages(senderID, payload);
+  processPayLoad(senderID, payload);
 }
 
 function receivedDeliveryConfirmation(event) {
@@ -153,7 +155,10 @@ function receivedMessage(event) {
   let text = event.message.text;
   let receivedMessage = event.message;
 
-  if (receivedMessage.attachments && receivedMessage.attachments[0].payload.url) {
+  if (
+    receivedMessage.attachments &&
+    receivedMessage.attachments[0].payload.url
+  ) {
     let attachedImgURL = receivedMessage.attachments[0].payload.url;
     console.log("Received image message : %s" + attachedImgURL);
 
@@ -166,41 +171,15 @@ function receivedMessage(event) {
       let aiTextAction = response.result.action;
       let aiTextResponse = response.result.fulfillment.speech;
 
-      console.log(
-        "\n****************************processing event****************************\n"
-      );
-      console.log("aiTextAction-->" + aiTextAction);
+      console.log("Returned from NLP API AI-->" + aiTextAction);
 
       switch (aiTextAction) {
-        case "case1":
-          console.log(
-            "\n\nswitch to Time Stamp :" + estTimeStamp + "\n"
-          );
+        case "input.welcome":
+          // sendLoginButton(sender);
+          sendWelcomeButton(sender);
           break;
 
-        case "input.welcome":
-          
-       // sendLoginButton(sender);
-        sendWelcomeButton(sender);
-
-
-
-        break;
         case "search":
-
-        //write whatever shit we want
-          if (
-            response.result.parameters["userSearchText"] ||
-            response.result.parameters["recommandType"]
-          ) {
-
-          } else {
-            let errorMessage = "please narrow down your search.";
-            prepareSendTextMessage(sender, errorMessage);
-
-            //ask users something to search
-          }
-
           break;
 
         default:
@@ -220,8 +199,9 @@ function receivedMessage(event) {
     apiaiSession.end();
   }
 }
-function sendLoginButton(recipientId){
 
+//pass array of template button templateElements /generic
+function sendLoginButton(recipientId, templateElements) {
   console.log(
     "[sendHelpOptionsAsButtonTemplates] Sending the help options menu"
   );
@@ -237,17 +217,18 @@ function sendLoginButton(recipientId){
   };
   var templateElements = [];
 
-  var oAuth_QBurl = "https://appcenter.intuit.com/connect/oauth2?client_id="+qb_client_id;
+  var oAuth_QBurl =
+    "https://appcenter.intuit.com/connect/oauth2?client_id=" + qb_client_id;
 
-    templateElements.push({
-      title: "Login to Your Quickbooks",
-      buttons:[
-        {
-          "type": "account_link",
-          "url": oAuth_QBurl
-        }
-      ]
-    });
+  templateElements.push({
+    title: "Login to Your Quickbooks",
+    buttons: [
+      {
+        type: "account_link",
+        url: oAuth_QBurl
+      }
+    ]
+  });
 
   var messageData = {
     recipient: {
@@ -268,11 +249,18 @@ function sendLoginButton(recipientId){
 }
 
 
-function sendWelcomeButton(recipientId){
+function  sectionButton(title, action, options) {
+  var payload = options | {};
+  payload = Object.assign(options, { action: action });
+  return {
+    type: "postback",
+    title: title,
+    payload: JSON.stringify(payload)
+  };
+};
 
-  console.log(
-    "[sendHelpOptionsAsButtonTemplates] Sending the help options menu"
-  );
+function sendButtonMessages(recipientId, templateElements) {
+  console.log("[sendButtonMessages] Sending the buttons ");
 
   var sectionButton = function(title, action, options) {
     var payload = options | {};
@@ -283,25 +271,7 @@ function sendWelcomeButton(recipientId){
       payload: JSON.stringify(payload)
     };
   };
-  var templateElements = [];
 
-    templateElements.push({
-      title: "Get My Company Info",
-      buttons: [
-        sectionButton("Company Name", "Company_Info", {          
-        })
-      ]
-    });
-
-    templateElements.push({
-      title: "What you like to do today",
-      buttons: [
-        sectionButton("Send Invoice", "Get_Invoice", {          
-        }),
-        sectionButton("Create Invoice", "Get_Quote", {
-        })
-      ]
-    });
   var messageData = {
     recipient: {
       id: recipientId
@@ -318,11 +288,64 @@ function sendWelcomeButton(recipientId){
   };
 
   sendMessagetoFB(messageData);
+}
+
+function sendWelcomeButton(recipientId) {
+  var templateElements = [];
+
+  templateElements.push({
+    title: "Get My Company Info",
+    buttons: [sectionButton("Company Name", "Company_Info", {})]
+  });
+
+  templateElements.push({
+    title: "What you like to do today",
+    buttons: [
+      sectionButton("Send Invoice", "Get_Invoice", {}),
+      sectionButton("Create Invoice", "Get_Quote", {})
+    ]
+  });
+
+  sendButtonMessages(recipientId, templateElements);
+
+
+  var templateElements = [];
+
+  templateElements.push({
+    title: "Get My Company Info",
+    buttons: [sectionButton("Company Name", "Company_Info", {})]
+  });
+
+  templateElements.push({
+    title: "What you like to do today",
+    buttons: [
+      sectionButton("Send Invoice", "Get_Invoice", {}),
+      sectionButton("Create Invoice", "Get_Quote", {})
+    ]
+  });
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: templateElements
+        }
+      }
+    }
+  };
+
   // });
 }
 
-
-function sendProductsOptionsAsButtonTemplates(recipientId, products,searchTag) {
+function sendProductsOptionsAsButtonTemplates(
+  recipientId,
+  products,
+  searchTag
+) {
   console.log(
     "[sendHelpOptionsAsButtonTemplates] Sending the help options menu"
   );
@@ -340,7 +363,7 @@ function sendProductsOptionsAsButtonTemplates(recipientId, products,searchTag) {
   products.forEach(function(product) {
     var url = HOST_URL + "products/" + product.handle;
     // console.log("Product url -\n" + url);
-    
+
     templateElements.push({
       title: product.title,
       subtitle: product.tags,
@@ -349,13 +372,17 @@ function sendProductsOptionsAsButtonTemplates(recipientId, products,searchTag) {
         {
           type: "web_url",
           url: url,
-          title: "Read description",
+          title: "Read description"
           // webview_height_ratio: "compact",
           // messenger_extensions: "true"
         },
-        sectionButton("Check avaliable Sizes and colors", "QR_GET_PRODUCT_OPTIONS", {
-          id: product.id
-        }),
+        sectionButton(
+          "Check avaliable Sizes and colors",
+          "QR_GET_PRODUCT_OPTIONS",
+          {
+            id: product.id
+          }
+        ),
         sectionButton("Check Price", "QR_GET_PRODUCT_PRICE", {
           id: product.id
         })
@@ -381,8 +408,7 @@ function sendProductsOptionsAsButtonTemplates(recipientId, products,searchTag) {
   // });
 }
 
-
-function sendButtonMessages(recipientId, requestForHelpOnFeature) {
+function processPayLoad(recipientId, requestForHelpOnFeature) {
   var templateElements = [];
   var requestPayload = JSON.parse(requestForHelpOnFeature);
   var sectionButton = function(title, action, options) {
@@ -404,56 +430,45 @@ function sendButtonMessages(recipientId, requestForHelpOnFeature) {
       payload: JSON.stringify(payload)
     };
   };
-  let payloadAction = requestPayload.action
+  let payloadAction = requestPayload.action;
   console.log("requestPayload.action" + payloadAction);
   switch (payloadAction) {
-
-  case "Company_Info" :
-
+    case "Company_Info":
       send_CompanyInfo(recipientId);
 
-        
-  break;
-  case "Send_Invoice":
-    var options = "";
-    var variants = "Trying to query QB API";
-
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        text: variants.substring(0, 640)
-
-      }
-    };
-    sendMessagetoFB(messageData);
-  // });
-
-  break;
-
-  case "Create_Invoice":
-  // var sh_product = shopify.product.get(requestPayload.id);
-  // sh_product.then(function(product) {
-    var options = "";
-    var variants = "Creating Invoice";
-
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        text: variants.substring(0, 640)
-
-      }
-    };
-    sendMessagetoFB(messageData);
-  // });
       break;
 
-      default:
-      // code to be executed if n is different from first 2 cases.
+    case "Send_Invoice":
+      var options = "";
+      var variants = "Trying to query QB API";
+
+      var variants = "Creating Invoice";
+      prepareTextMessage(recipientId, variants, "");
+
+      break;
+
+    case "Create_Invoice":
+      var variants = "Creating Invoice";
+      prepareTextMessage(recipientId, variants, "");
+
+      // });
+      break;
+
+    default:
+    // code to be executed if n is different from first 2 cases.
   }
+}
+
+function prepareTextMessage(recipientId, variants, options) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: variants.substring(0, 640)
+    }
+  };
+  sendMessagetoFB(messageData);
 }
 
 function sendMessagetoFB(messageData) {
@@ -480,73 +495,55 @@ function prepareSendTextMessage(sender, aiText) {
   sendMessagetoFB(messageData);
 }
 
-
-
-function send_CompanyInfo(recipientId){
-   
-    // Set up API call (with OAuth2 accessToken)
-    var qb_url = config.api_uri + config.realmId + '/companyinfo/' + config.realmId
-    console.log('Making API call to: ' + qb_url)
-    var requestObj = {
-      url: qb_url,
-      headers: {
-        'Authorization': 'Bearer ' + config.qb_access_token,
-        'Accept': 'application/json'
-      }
+function send_CompanyInfo(recipientId) {
+  // Set up API call (with OAuth2 accessToken)
+  var qb_url =
+    config.api_uri + config.realmId + "/companyinfo/" + config.realmId;
+  console.log("Making API call to: " + qb_url);
+  var requestObj = {
+    url: qb_url,
+    headers: {
+      Authorization: "Bearer " + config.qb_access_token,
+      Accept: "application/json"
     }
-  
-    // Make API call
-    request(requestObj, function (err, response) {
-      // Check if 401 response was returned - refresh tokens if so!
-        if(err || response.statusCode != 200) {
-          return res.json({error: err, statusCode: response.statusCode})
-        }
+  };
 
-        var responseData= JSON.parse(response.body);
-        console.log('company response.body: ' + JSON.stringify(response.body))
-        // API Call was a success!
-        //res.json(JSON.parse(response.body))
-
-        var options = "";
-        var variants = responseData.CompanyInfo.CompanyName;
-    
-        var messageData = {
-          recipient: {
-            id: recipientId
-          },
-          message: {
-            text: variants.substring(0, 640)
-    
-          }
-        };
-        sendMessagetoFB(messageData);
-      });
-  
+  // Make QB  API call
+  request(requestObj, function(err, response) {
+    // Check if 401 response was returned - refresh tokens if so!
+    if (err || response.statusCode != 200) {
+      return res.json({ error: err, statusCode: response.statusCode });
     }
 
-/* Webhook for API.ai to get response from the 3rd party API */
-app.post("/ai", (req, res) => {
-  var templateElements = [];
-  switch (req.body.result.action) {
-      
-    case "shipping":
-      console.log("\n\n*** Shipping *** Time Stamp :" + estTimeStamp + "\n");
-      let address = req.body.result.parameters["geo-country"];
+    var responseData = JSON.parse(response.body);
+    console.log("company response.body: " + JSON.stringify(response.body));
+  
+    var variants = responseData.CompanyInfo.CompanyName;
+    prepareTextMessage(recipientId, variants, "");
+  });
+}
 
+// /* Webhook for API.ai to get response from the 3rd party API */
+// app.post("/ai", (req, res) => {
+//   var templateElements = [];
+//   switch (req.body.result.action) {
+//     case "shipping":
+//       console.log("\n\n*** Shipping *** Time Stamp :" + estTimeStamp + "\n");
+//       let address = req.body.result.parameters["geo-country"];
 
-      break;
+//       break;
 
-    case "search":
-      console.log("\n case - search");
-      let msg = "Converted Text to JSON";
-      return res.json({
-        speech: msg,
-        displayText: msg,
-        source: "search"
-      });
-      break;
+//     case "search":
+//       console.log("\n case - search");
+//       let msg = "Converted Text to JSON";
+//       return res.json({
+//         speech: msg,
+//         displayText: msg,
+//         source: "search"
+//       });
+//       break;
 
-    default:
-    // code to be executed if n is different from first 2 cases.
-  }
-});
+//     default:
+//     // code to be executed if n is different from first 2 cases.
+//   }
+// });
